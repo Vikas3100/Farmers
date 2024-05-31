e1b9fbb288da2c59703bee9273b3251a778a34a2
Vikas Yadav <139801905+Vikas3100@users.noreply.github.com>
2024-05-31 23:39:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Farmers
+# Farmers-website
+A farming website made with Bootstrap 4 
---
+++ fractorial.py
@@ -0,0 +1,6 @@
+my_dict = {"name": "Ram", "age": 25}
+new_dict = my_dict
+new_dict["name"] = "vikas"
+
+print(my_dict) 
+print(new_dict)
